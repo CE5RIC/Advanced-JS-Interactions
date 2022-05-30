a42b46b86b43cad50c195375444b00eaae83f47e
CE5RIC <cedricdierckx5@hotmail.com>
2022-05-30 11:32:22 +0200

    function created to enlarge image by click

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ function randomLetter(){
     const alphabet = "abcdefghijklmnopqrstuvwxyz"
     return alphabet[Math.floor(Math.random() * alphabet.length)]
 }
+
+
+// Function created to enlarge movie image on click
+
+document.getElementById("enlarge").addEventListener("click", function() {
+    img = document.getElementById("enlarge");
+    img.style.transform = "scale(1.5)";
+    img.style.transition = "transform 0.25s ease";
+})
+
+
+
+
+
+
+
+
+
