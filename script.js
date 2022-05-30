bbd103be071407ef6f7d9ee2970a375ac97002c9
CE5RIC <cedricdierckx5@hotmail.com>
2022-05-30 18:19:41 +0200

    Writing more efficient code for enlarging images

--- script.js
+++ script.js
@@ -8,13 +8,75 @@ function randomLetter(){
 }
 
 
-// Function created to enlarge movie image on click
+// trying to make something more efficient for enlarging images
+
+let moviePosters = document.getElementsByClassName("image");
+
+imagesArray = [];
+
+imagesArray.push(moviePosters);
+
+
+console.log(imagesArray);
+
+
+// function new to enlarge image
+
+
+
+
+
+/*
 
 document.getElementById("enlarge").addEventListener("click", function() {
-    img = document.getElementById("enlarge");
-    img.style.transform = "scale(1.5)";
-    img.style.transition = "transform 0.25s ease";
+   enlargeImage();
+
 })
+
+
+
+document.getElementById("close").addEventListener("click", function() {
+    resetImageSize();
+})
+
+
+// Function to enlarge Image 
+
+function enlargeImage() {
+    img = document.getElementById("enlarge");
+    img.style.transform = "scale(2)";
+    img.style.transition = "transform 0.25s ease";
+
+
+    console.log("large");
+
+    return;
+}
+
+
+
+// function to reset Image size
+
+function resetImageSize() {
+  
+    img = document.getElementById("close");
+    img.style.transform = "scale(1)";
+    img.style.transition = "transform 0.25s ease";
+
+    console.log("reset");
+
+}
+
+
+
+
+*/
+
+
+
+
+    
+
 
 
 
